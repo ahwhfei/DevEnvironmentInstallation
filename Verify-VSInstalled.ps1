4979698cacc712acc384daf186a20e50f6b5c2ba
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-08 20:14:25 +0800

    add more detail about return code of vs installer

--- Verify-VSInstalled.ps1
+++ Verify-VSInstalled.ps1
@@ -6,9 +6,23 @@ do {
 } until ([string]::IsNullOrWhiteSpace($result))
 
 Write-Host ""
-if ($LASTEXITCODE -eq 0) {
+if (($LASTEXITCODE -eq 0) -or ($LASTEXITCODE -eq 0x00000bc2)) {
+    if ($LASTEXITCODE -eq 0x00000bc2) {
+        Write-Host -ForegroundColor Green "Need to reboot to take effect."
+    }
     Write-Host -ForegroundColor Green "Visual Studio Instatllation Comoplete"
 } else {
+    if (($LASTEXITCODE -eq 0x80044000) -or ($LASTEXITCODE -eq 0x8004C000)) {
+        Write-Host "Block"
+    }
+    elseif (($LASTEXITCODE -eq 0x00000642) -or ($LASTEXITCODE -eq 0x80048642)) {
+        Write-Host "Cancel"
+    }
+    elseif ($LASTEXITCODE -eq 0x80048bc7) {
+        Write-Host "Incomplete-Reboot Required"
+    }
+    elseif (($LASTEXITCODE -eq 0x00000643) -or ($LASTEXITCODE -eq 0x80048643)) {
+        Write-Host "Failure"
+    }
     Write-Host -ForegroundColor Yellow "WARNING: Visual Studio may not installed"
 }
-
